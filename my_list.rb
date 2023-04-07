1593ae7f235d865f6abb5f145c65f2710bdbc5c0
Shakir Hussain <shakir.hussain6969@gmail.com>
2023-04-08 01:48:49 +0500

    Fixed Linters and Added tests

--- my_list.rb
+++ my_list.rb
@@ -28,4 +28,10 @@ mylist = [1, 2, 3, 4, 5]
 
 list = MyList.new(mylist)
 
-list.all? { |x| x > 2 }
+puts list.all? { |x| x > 2 }
+
+puts list.any? { |x| x > 2 }
+
+print list.filter { |x| x > 2 }
+puts ' '
+print list.reverse_em
